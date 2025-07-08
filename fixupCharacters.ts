fce5ecb3affa5d0fe98eedcf8f900185c7610b41
Tobias Berger <toby@tobot.dev>
2025-07-08 09:54:38 +0200

    Fix up characters

---
+++ fixupCharacters.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env bun
+import { $ } from "bun";
+import CHARACTERS from "lib/characters.json";
+import type { Character } from "lib/util";
+
+const newCharacters: Character[] = [];
+
+for (const character of CHARACTERS) {
+	newCharacters.push({
+		name: character.name,
+		homeWorld: character.homeWorld,
+		firstAppearance: character.firstAppearance,
+		species: character.species,
+		abilities: character.abilities.sort(),
+		id: character.id,
+		validFrom: character.validFrom,
+		validUntil: character.validUntil,
+	});
+}
+
+Bun.file("./src/lib/characters.json").write(
+	JSON.stringify(newCharacters, null, 2),
+);
+for await (const line of $`nix fmt`.lines()) {
+	console.log(line);
+}
